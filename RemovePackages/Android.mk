1dbbe4551568190e41d7dbc7f7e01ab2c526a5b3
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-02-02 20:11:08 +0530

    Update Android.mk

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := GoogleCamera MyVerizonServices OPScreenRecord PixelWallpapers2020 PixelLiveWallpaperPrebuilt  ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt WallpapersBReel2020 NfcNci MaestroPrebuilt TipsPrebuilt GoogleFeedback AppDirectedSMSService ConnMO DCMO USCCDM Turbo TurboPrebuilt TurboAdapter Videos Camera2 Safety
+LOCAL_OVERRIDES_PACKAGES := GoogleCamera MyVerizonServices OPScreenRecord PixelWallpapers2020 PixelLiveWallpaperPrebuilt  ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt WallpapersBReel2020 NfcNci MaestroPrebuilt TipsPrebuilt GoogleFeedback AppDirectedSMSService ConnMO DCMO USCCDM TurboPrebuilt TurboAdapter Videos Camera2 Safety
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
